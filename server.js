537096dc734d7eb8791b6867beb040f9d0a94364
Maggie Abernathy <mlabernathy05@gmail.com>
2023-01-11 21:56:49 -0600

    main menu prompt added using inquirer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql12');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,6 +17,46 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
 );
+
+function appMenu() {
+    console.log("Welcome to your employee directory!")
+    inquirer
+        .createPromptModule([
+            {
+                type: 'list',
+                name: 'direction',
+                message: "Please choose an option below:",
+                choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+            },
+        ])
+        .then((userChoice) => {
+            switch(userChoice.direction) {
+                case 'View all Departments':
+                    console.log('function for dept.')
+                    break;
+                case 'View all Roles':
+                    console.log('function for roles')
+                    break;
+                case 'View all Employees':
+                    console.log('view empl')
+                    break;
+                case 'Add a Department':
+                    console.log('add dept')
+                    break;
+                case 'Add a Role':
+                    console.log('add role')
+                    break;
+                case 'Add an Employee':
+                    console.log('add Employee')
+                    break;
+                case 'Update an Employee Role':
+                    console.log('update emp')
+                    break;
+                default:
+                    console.log('pull directory');
+            }
+        })
+}
 
 app.use((req,res) => {
     res.status(404).end();
