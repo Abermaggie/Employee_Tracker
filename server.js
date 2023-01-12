c818aa56fbb160ea132741c1cfefa371a755cde3
Maggie Abernathy <mlabernathy05@gmail.com>
2023-01-12 14:37:08 -0600

    constructor functions finished, inquirer prompts written

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const Department = require('./lib/department');
+const Roles = require('./lib/roles');
+const Employee = require('./lib/employees');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -33,72 +36,154 @@ const getEmp = () => {db.query('SELECT * FROM employee', function (err, results)
 })
 };
 
-getRole();
-getEmp();
-getDept();
-// const deptList = [];
-// const deptId= [];
-// const empId= [];
+// getRole();
+// getEmp();
+// getDept();
+const deptList = [];
+const roleList= [];
+const empList= [];
 
 
 
-// function appMenu() {
-//     console.log("Welcome to your employee directory!")
-//     inquirer
-//         .createPromptModule([
-//             {
-//                 type: 'list',
-//                 name: 'direction',
-//                 message: "Please choose an option below:",
-//                 choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
-//             },
-//         ])
-//         .then((userChoice) => {
-//             switch(userChoice.direction) {
-//                 case 'View all Departments':
-//                     console.log('function for dept.')
-//                     break;
-//                 case 'View all Roles':
-//                     console.log('function for roles')
-//                     break;
-//                 case 'View all Employees':
-//                     console.log('view empl')
-//                     break;
-//                 case 'Add a Department':
-//                     console.log('add dept')
-//                     break;
-//                 case 'Add a Role':
-//                     console.log('add role')
-//                     break;
-//                 case 'Add an Employee':
-//                     console.log('add Employee')
-//                     break;
-//                 case 'Update an Employee Role':
-//                     console.log('update emp')
-//                     break;
-//                 default:
-//                     console.log('pull directory');
-//             }
-//         })
-// };
+function appMenu() {
+    console.log("Welcome to your employee directory!")
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'direction',
+                message: "Please choose an option below:",
+                choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+            }
+        ])
+        .then((userChoice) => {
+            switch(userChoice.direction) {
+                case 'View all Departments':
+                    getDept()
+                    break;
+                case 'View all Roles':
+                    getRole();
+                    break;
+                case 'View all Employees':
+                    getEmp();
+                    break;
+                case 'Add a Department':
+                    addDept()
+                    break;
+                case 'Add a Role':
+                    addRole();
+                    break;
+                case 'Add an Employee':
+                    addEmp();
+                    break;
+                case 'Update an Employee Role':
+                    console.log('update emp')
+                    break;
+                default:
+                    console.log('pull directory');
+            }
+        })
+};
 
-// function addDept() {
-//     console.log("Please enter department information.");
-//     inquirer
-//         .prompt([
-//             {
-//                 type: 'input',
-//                 name: 'deptName',
-//                 message: "What is the name of the department you would like to add?"
-//             }
-//         ])
-//         .then((answers) => {
-//             const department = new Department(
-//                 answers.deptName,
-//             );
+function addDept() {
+    console.log("Please enter department information.");
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'deptId',
+                message: "What is the ID of the department you would like to add?"
+            },
+            {
+                type: 'input',
+                name: 'deptName',
+                message: "What is the name of the department you would like to add?"
+            }
+        ])
+        .then((answers) => {
+            const department = new Department(
+                answers.deptId,
+                answers.deptName,
+            );
+            deptList.push(department);
+            console.log(deptList);
+        })
+}
 
-//         })
-// }
+function addRole() {
+    console.log("Please enter role information.");
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'roleName',
+                message: "What is the name of the role you would like to add?"
+            },
+            {
+                type: 'input',
+                name: 'roleId',
+                message: "What is the ID # of this role?"
+            },
+            {
+                type: 'input',
+                name: 'roleSal',
+                message: "What is the minimum salary of this role?"
+            },
+            {
+                type: 'input',
+                name: 'roleDept',
+                message: "What is the name of the department this role will be added to?"
+            }
+        ])
+        .then((answers) => {
+            const roles = new Roles(
+                answers.roleId,
+                answers.roleName,
+                answers.roleDept,
+                answers.roleSal
+            );
+            roleList.push(roles);
+            console.log(roleList);
+        })
+    }
+
+function addEmp() {
+    console.log("Please enter your new employee's information.");
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'empFName',
+                message: "What is the first name of the new Employee?"
+            },
+            {
+                type: 'input',
+                name: 'empLName',
+                message: "What is the last name of the new Employee?"
+            },
+            {
+                type: 'input',
+                name: 'empRole',
+                message: "What is the role of the new Employee?"
+            },
+            {
+                type: 'input',
+                name: 'empMgr',
+                message: "Who is this employee's direct Manager?"
+            }
+        ])
+        .then((answers) => {
+            const employee = new Employee (
+                answers.empFName,
+                answers.empLName,
+                answers.empRole,
+                answers.empMgr
+            );
+            empList.push(employee);
+            console.log(empList);
+        })
+    };
+appMenu();
 
 app.use((req,res) => {
     res.status(404).end();
--- sql_folder/seeds.sql
+++ sql_folder/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO department (id, name)
-VALUES (1, "PHYSICS"),
+VALUES (0, "PHYSICS"),
     (2, "CHEMISTRY"),
     (3, "HOME ECONOMICS"),
     (4, "PSYCHOLOGY");
